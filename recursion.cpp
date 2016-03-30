7289bf907d7a9d359c057fba3a2de254104fa492
Daniel Méndez <danielmendez310@gmail.com>
2016-03-30 11:01:12 -0300

    Create recursion.cpp

---
+++ recursion.cpp
@@ -0,0 +1,20 @@
+/*
+Ejercicio: Programe recursivamente un método que me indique si un string es
+palíndrome.
+
+Ejercicio: Defina recursivamente la función P(n) que define el
+número de palíndromes de largo n.
+
+Ejercicio: Sea la siguiente función definida recursivamente como
+f: NxN -> N
+f(0,y) = y+1
+f(x+1,0) = f(x,1)
+f(x+1,y+1) = f(x,f(x+1,y))
+
+Programela. (RECURSIVAMENTE... Dah!)
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
