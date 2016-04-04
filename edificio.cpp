ff1174aa7b8170306782285775757743a62802ed
Crepu <danielmendez310@gmail.com>
2016-04-04 03:41:15 -0300

    Enunciado control 1

---
+++ edificio.cpp
@@ -0,0 +1,84 @@
+/*
+El conserje de un edificio está muy atareado con el cobro 
+de los gastos comunidades de los habitantes del edificio. 
+Le pide que le ayude a con la organización de los departamentos 
+del edificio y para eso le brinda la siguiente estructura de datos:
+
+El conserje necesita saber algunas cosas específicas del edicio, 
+las cuales usted debe implementar. Estas son:
+
+	* Deuda total del edicio.
+	* Gastos mensuales de un piso dado.
+	* Cantidad de departamentos desocupados de edificio.
+
+Se brinda como ayuda el siguiente esquema de un piso del edificio.
+
+
+
+-----------------------------
+|1	|2	|3	 |... |... |...	|
+|___|___|____|____|____|____|
+|	PASILLO			   |...	|
+|______________________|____|
+|N	|...|... |... |... |...	|
+|	|	|	 |	  |	   |	|
+-----------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class Departamento{
+
+	string dueño;
+	int piso;
+	bool ocupado;
+
+	float agua_mes, gas_mes, internet_mes, corriente_mes;
+	float agua_deuda, gas_deuda, internet_deuda, corriente_deuda;
+
+	Departamento *arriba;
+	Departamento *abajo;
+
+	Departamento *depto_sig;
+	Departamento *depto_ant;
+
+
+
+float deuda_total_edificio(Departamento *p){
+	Departamento *aux = p;
+	while(aux->depto_ant != NULL){
+		aux = aux->depto_ant;
+	}
+	while(aux->abajo != NULL){
+		aux = aux->abajo;
+	}
+	float suma = 0;
+	Departamento *inicio = aux;
+	while(aux && aux->arriba != NULL){
+		
+		while(aux && aux->depto_sig != NULL){
+			suma = aux->agua_deuda + aux->agua_mes + suma;
+			aux = aux->depto_sig;
+		}
+
+	aux = inicio->arriba;
+	}
+
+
+
+}
+
+
+
+float gastos_mensuales_piso(int p){
+
+}
+
+int departamentos_desocupados(Departamento *p){
+
+}
+
+};
---
+++ recursion.cpp
@@ -0,0 +1,32 @@
+/*
+Ejercicio: Programe recursivamente un método que me indique si un string es
+palíndrome.
+
+Ejercicio: Defina recursivamente la función P(n) que define el
+número de palíndromes de largo n.
+
+Ejercicio: Sea la siguiente función definida recursivamente como
+f: NxN -> N
+f(0,y) = y+1
+f(x+1,0) = f(x,1)
+f(x+1,y+1) = f(x,f(x+1,y))
+
+Programela. (RECURSIVAMENTE... Dah!)
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int foo(int x, int y){
+	if(x == 0) return y+1;
+	if(y == 0) return foo(x-1,1);
+	return foo(x-1,foo(x,y-1));
+}
+
+int main()
+{
+	cout<<foo(3,1)<<endl;
+	return 0;
+}
